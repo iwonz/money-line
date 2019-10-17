4c558e7e0b553b7efa835a11dd569b513840d2e6
iwonz <iwonz@yandex.ru>
2019-10-17 22:47:09 +0300

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import TimeLine from "../Timeline/TimeLine";
+
+import moment from 'moment';
+
+import 'moment/min/locales';
+
+moment.locale(window.navigator.language);
+
+const App: React.FC = () => {
+    return (
+        <div className="app">
+            <TimeLine/>
+        </div>
+    );
+}
+
+export default App;
