3403721a4542b50594fa9b5b6bdaeb0d46cb3857
iwonz <iwonz@yandex.ru>
2019-10-22 23:31:51 +0300

    Updates

---
+++ precache-manifest.4bffb5b3ad8cd8b90668992e150514e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a498f5fc846c2c4ce8b051c7f5d14d6",
+    "url": "/iwonz/money-line/index.html"
+  },
+  {
+    "revision": "3e693df66d968a2c4f9f",
+    "url": "/iwonz/money-line/static/css/main.45e49e4d.chunk.css"
+  },
+  {
+    "revision": "931a61a2c8043b6d72f9",
+    "url": "/iwonz/money-line/static/js/2.1f12ee61.chunk.js"
+  },
+  {
+    "revision": "3e693df66d968a2c4f9f",
+    "url": "/iwonz/money-line/static/js/main.b8d25e5e.chunk.js"
+  },
+  {
+    "revision": "7650c3326f36044eada4",
+    "url": "/iwonz/money-line/static/js/runtime-main.b3bcec60.js"
+  }
+]);
